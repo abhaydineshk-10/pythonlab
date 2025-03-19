55a0a092125a33de7803a9ea37935f9939ea3cf8
Abhay Dinesh <eyeronic2772@gmail.com>
2025-03-19 14:18:36 +0530

    Add files via upload

---
+++ fibboseq.py
@@ -0,0 +1,10 @@
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+# Example usage
+fib_gen = fibonacci_generator()
+for _ in range(10):  # Generate the first 10 Fibonacci numbers
+    print(next(fib_gen))
